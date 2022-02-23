adc79f7f0dfbfcb7e569bb0c6610e4695451aada
KING-PIN <mohdathar1991@gmail.com>
2022-02-23 18:22:54 +0530

    added sticky notes with dragndrop

--- tools.js
+++ tools.js
@@ -4,6 +4,7 @@ let pencilOptionsContainer = document.querySelector(".pencil-options-container")
 let eraserOptionsContainer = document.querySelector(".eraser-options-container");
 let pencilImage = document.querySelector(".pencil-image");
 let eraserImage = document.querySelector(".eraser-image");
+let sticky = document.querySelector(".notes-image")
 let optionsFlag = true;
 let eraserFlag = false;
 let pencilFlag = false;
@@ -42,3 +43,46 @@ eraserImage.addEventListener("click" , (e) => {
     if(!eraserFlag) eraserOptionsContainer.style.display = "flex";
     else    eraserOptionsContainer.style.display = "none";
 })
+
+sticky.addEventListener("click" , (e) => {
+    let stickyContainer = document.createElement("div");
+    stickyContainer.setAttribute("class" ,"sticky-notes-container");
+    stickyContainer.innerHTML = `
+    <div class="header">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="content-container">
+        <textarea></textarea>
+    </div>
+    `;
+    document.body.appendChild(stickyContainer);
+
+    stickyContainer.onmousedown = function(event) {
+        dragNDrop(stickyContainer,event);
+      };
+    
+    stickyContainer.ondragstart = function() {
+        return false;
+      };
+})
+
+function dragNDrop(element,event){
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+        let shiftY = event.clientY - element.getBoundingClientRect().top;
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+        moveAt(event.pageX, event.pageY);
+        function moveAt(pageX, pageY) {
+            element.style.left = pageX - shiftX + 'px';
+            element.style.top = pageY - shiftY + 'px';
+        }
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
+        }
+        document.addEventListener('mousemove', onMouseMove);
+        element.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          element.onmouseup = null;
+        };
+}
