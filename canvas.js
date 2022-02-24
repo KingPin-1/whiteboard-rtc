208aeeafa597013ea18ade1ae89084e388b14ccf
KING-PIN <mohdathar1991@gmail.com>
2022-02-24 12:11:27 +0530

    styled pencil colors

--- canvas.js
+++ canvas.js
@@ -5,25 +5,54 @@ canvas.height = window.innerHeight;
 //API
 let tool = canvas.getContext("2d");
 
-tool.strokeStyle = "black";
-tool.lineWidth = "3";
+let pencilColorAll = document.querySelectorAll(".pencil-color");
+let pencilWidthElem = document.querySelector(".pencil-width");
+let eraserWidthElem = document.querySelector(".eraser-width");
+
+let pencilColor = "black";
+let eraserColor = "white";
+let pencilWidth = pencilWidthElem.value ;
+let eraserWidth = eraserWidthElem.value ;
+
+tool.strokeStyle = pencilColor;
+tool.lineWidth = pencilWidth;
+
+pencilColorAll.forEach(colorElem => {
+    colorElem.addEventListener("click" , (e) =>{
+        let chosenColor = colorElem.classList[1];
+        pencilColor = chosenColor;
+        tool.strokeStyle = chosenColor;
+    })
+})
 
 let mouseDownFlag = false;
 
 canvas.addEventListener("mousedown" , (e) => {
     mouseDownFlag = true;
-    tool.beginPath();
-    tool.moveTo(e.clientX , e.clientY);
+    beginPath({
+        x:e.clientX ,
+        y:e.clientY
+    })
+    
 })
-
 canvas.addEventListener("mousemove" , (e) =>{
     if(mouseDownFlag){
-        tool.lineTo(e.clientX , e.clientY);
-        tool.stroke();  
+        drawStroke({
+            x:e.clientX ,
+            y:e.clientY
+        })
     }
 })
-
 canvas.addEventListener("mouseup" , (e) => {
     mouseDownFlag = false;
 })
 
+function beginPath(strokeObj){
+    tool.beginPath();
+    tool.moveTo(strokeObj.x , strokeObj.y);
+}
+
+function drawStroke(strokeObj){
+    tool.lineTo(strokeObj.x , strokeObj.y);
+    tool.stroke();
+}
