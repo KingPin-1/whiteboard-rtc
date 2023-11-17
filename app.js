ec1ed71459659485a05a0d95afed5e8a8d92492e
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-11-18 03:00:21 +0530

    Updated Project

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-const express = require("express"); //Access
-const socket = require("socket.io");
+const express = require('express'); //Access
+const socket = require('socket.io');
 
 const app = express(); //Iniitialized and server ready
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json());
 
-let port = process.env.PORT || 5000 ;
-let server = app.listen(port , () => {
-    console.log("Listening to Port " + port);   
-})
+let port = process.env.PORT || 5000;
+let server = app.listen(port, () => {
+    console.log('Listening to Port ' + port);
+});
 
-let io = socket(server);    
-io.on("connection" , (socket)=>{
-    console.log("Made socket Connection");
+let io = socket(server);
+io.on('connection', (socket) => {
+    console.log('Made socket Connection');
     //data from front-end
     //received data
-    socket.on("beginPath" , (data) => {
+    socket.on('beginPath', (data) => {
         //Transfer data here to all connected computers.
-        io.sockets.emit("beginPath" , data);
-    })
+        io.sockets.emit('beginPath', data);
+    });
 
-    socket.on("drawStroke" , (data) => {
-        io.sockets.emit("drawStroke" , data);
-    })
+    socket.on('drawStroke', (data) => {
+        io.sockets.emit('drawStroke', data);
+    });
 
-    socket.on("redoUndo" , (data)=>{
-        io.sockets.emit("redoUndo",data);
-    })
-})
+    socket.on('redoUndo', (data) => {
+        io.sockets.emit('redoUndo', data);
+    });
+});
--- public/canvas.js
+++ public/canvas.js
@@ -1,154 +1,144 @@
-let canvas = document.querySelector("canvas");
+let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 //API
-let tool = canvas.getContext("2d");
+let tool = canvas.getContext('2d');
 
-let pencilColorAll = document.querySelectorAll(".pencil-color");
-let pencilWidthElem = document.querySelector(".pencil-width");
-let eraserWidthElem = document.querySelector(".eraser-width");
+let pencilColorAll = document.querySelectorAll('.pencil-color');
+let pencilWidthElem = document.querySelector('.pencil-width');
+let eraserWidthElem = document.querySelector('.eraser-width');
 
-let pencilColor = "black";
-let eraserColor = "white";
-let pencilWidth = pencilWidthElem.value ;
-let eraserWidth = eraserWidthElem.value ;
+let pencilColor = 'black';
+let eraserColor = 'white';
+let pencilWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
 
-let download = document.querySelector(".download-image");
-let undo = document.querySelector(".undo-image");
-let redo = document.querySelector(".redo-image");
+let download = document.querySelector('.download-image');
+let undo = document.querySelector('.undo-image');
+let redo = document.querySelector('.redo-image');
 
-let undoRedoTracker = [] ; // keeps all actions performed 
-let tracker = 0 ; //which action from tracker array
+let undoRedoTracker = []; // keeps all actions performed
+let tracker = 0; //which action from tracker array
 
 tool.strokeStyle = pencilColor;
 tool.lineWidth = pencilWidth;
 
-undo.addEventListener("click" , (e) => {
-    if( tracker > 0 ) tracker--;
-    // console.log(tracker);
+undo.addEventListener('click', (e) => {
+    if (tracker > 0) tracker--;
     let data = {
-        trackValue : tracker , 
-        undoRedoTracker
-    } 
-    socket.emit("redoUndo",data);
-    // undoRedoCanvas(trackObj);
-})
+        trackValue: tracker,
+        undoRedoTracker,
+    };
+    socket.emit('redoUndo', data);
+});
 
-redo.addEventListener("click" , (e) => {
-    if( tracker < undoRedoTracker.length - 1 ) tracker++;
+redo.addEventListener('click', (e) => {
+    if (tracker < undoRedoTracker.length - 1) tracker++;
     let data = {
-        trackValue : tracker , 
-        undoRedoTracker
-    } 
-    socket.emit("redoUndo",data);
-    // undoRedoCanvas(trackObj);
-})
+        trackValue: tracker,
+        undoRedoTracker,
+    };
+    socket.emit('redoUndo', data);
+});
 
-function undoRedoCanvas(trackObj){
+function undoRedoCanvas(trackObj) {
     tracker = trackObj.trackValue;
     undoRedoTracker = trackObj.undoRedoTracker;
 
     let url = undoRedoTracker[tracker];
-    // console.log(tracker);
     let img = new Image();
     img.src = url;
     img.onload = (e) => {
-        tool.drawImage(img ,0 ,0 , canvas.width , canvas.height);
-    }
+        tool.clearRect(0, 0, canvas.width, canvas.height);
+        tool.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
 }
 
-
-pencilColorAll.forEach(colorElem => {
-    colorElem.addEventListener("click" , (e) =>{
+pencilColorAll.forEach((colorElem) => {
+    colorElem.addEventListener('click', (e) => {
         let chosenColor = colorElem.classList[1];
         pencilColor = chosenColor;
         tool.strokeStyle = chosenColor;
-    })
-})
+    });
+});
 
-pencilWidthElem.addEventListener("change" , (e) =>{
+pencilWidthElem.addEventListener('change', (e) => {
     pencilWidth = pencilWidthElem.value;
     tool.lineWidth = pencilWidth;
-})
+});
 
-eraserWidthElem.addEventListener("change" , (e) =>{
+eraserWidthElem.addEventListener('change', (e) => {
     eraserWidth = eraserWidthElem.value;
     tool.lineWidth = eraserWidth;
-})
+});
 
-eraserImage.addEventListener("click" , (e)=>{
-    if(eraserFlag){
+eraserImage.addEventListener('click', (e) => {
+    if (eraserFlag) {
         tool.strokeStyle = eraserColor;
         tool.lineWidth = eraserWidth;
-    }else{
+    } else {
         tool.strokeStyle = pencilColor;
         tool.lineWidth = pencilWidth;
     }
-})
+});
 
 let mouseDownFlag = false;
 
-canvas.addEventListener("mousedown" , (e) => {
+canvas.addEventListener('mousedown', (e) => {
     mouseDownFlag = true;
-    // beginPath({
-    //     x:e.clientX ,
-    //     y:e.clientY
-    // })
     let data = {
-        x:e.clientX ,
-        y:e.clientY
-    }
-    //send data to server
-    socket.emit("beginPath" , data);
-    
-})
-canvas.addEventListener("mousemove" , (e) =>{
-    if(mouseDownFlag){
+        x: e.clientX,
+        y: e.clientY,
+    };
+    socket.emit('beginPath', data);
+});
+canvas.addEventListener('mousemove', (e) => {
+    if (mouseDownFlag) {
         let data = {
-            x:e.clientX ,
-            y:e.clientY ,
-            color : eraserFlag ? eraserColor : pencilColor ,
-            width : eraserFlag ? eraserWidth : pencilWidth
-        }
-        socket.emit("drawStroke" , data);
+            x: e.clientX,
+            y: e.clientY,
+            color: eraserFlag ? eraserColor : pencilColor,
+            width: eraserFlag ? eraserWidth : pencilWidth,
+        };
+        socket.emit('drawStroke', data);
     }
-})
-canvas.addEventListener("mouseup" , (e) => {
+});
+canvas.addEventListener('mouseup', (e) => {
     mouseDownFlag = false;
 
     let url = canvas.toDataURL();
     undoRedoTracker.push(url);
-    tracker = undoRedoTracker.length-1;
-})
+    tracker = undoRedoTracker.length - 1;
+});
 
-function beginPath(strokeObj){
+function beginPath(strokeObj) {
     tool.beginPath();
-    tool.moveTo(strokeObj.x , strokeObj.y);
+    tool.moveTo(strokeObj.x, strokeObj.y);
 }
 
-function drawStroke(strokeObj){
-    tool.lineTo(strokeObj.x , strokeObj.y);
+function drawStroke(strokeObj) {
+    tool.lineTo(strokeObj.x, strokeObj.y);
     tool.strokeStyle = strokeObj.color;
     tool.lineWidth = strokeObj.width;
     tool.stroke();
 }
 
-download.addEventListener("click" , (e) => {
+download.addEventListener('click', (e) => {
     let url = canvas.toDataURL();
-    let a = document.createElement("a");
+    let a = document.createElement('a');
     a.href = url;
-    a.download = "myBoard.jpg";
+    a.download = 'myBoard.jpg';
     a.click();
-})
+});
 
-socket.on("beginPath" , (data) =>{
+socket.on('beginPath', (data) => {
     beginPath(data);
-})
-socket.on("drawStroke", (data) =>{
+});
+socket.on('drawStroke', (data) => {
     drawStroke(data);
-})
+});
 
-socket.on("redoUndo",(data)=>{
+socket.on('redoUndo', (data) => {
     undoRedoCanvas(data);
-})
+});
